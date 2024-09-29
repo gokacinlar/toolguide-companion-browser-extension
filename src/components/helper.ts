2b7c8319c5b5093067b9acf5be041babd9b4c574
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-09-29 20:33:25 +0300

    Include helper.js to prevent code duplication.

---
+++ src/components/helper.ts
@@ -0,0 +1,13 @@
+// Create a template content to be appended to Shadow DOM
+export class Template {
+    public createTemplate(styles: string, content: string): HTMLTemplateElement {
+        const template = document.createElement("template");
+        template.innerHTML = `
+            <style>
+                ${styles}
+            </style>
+            ${content}
+        `;
+        return template;
+    }
+}
