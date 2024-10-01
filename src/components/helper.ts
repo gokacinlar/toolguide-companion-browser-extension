8ed9fd7053e852ab9d3ab5bc96bd3692a7d60fd0
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-10-02 01:33:19 +0300

    Fix: change createTemplate() parameter to "any"

--- src/components/helper.ts
+++ src/components/helper.ts
@@ -1,6 +1,6 @@
 // Create a template content to be appended to Shadow DOM
 export class Template {
-    public createTemplate(styles: string, content: string): HTMLTemplateElement {
+    public createTemplate(styles: string, content: any): HTMLTemplateElement {
         const template = document.createElement("template");
         template.innerHTML = `
             <style>
