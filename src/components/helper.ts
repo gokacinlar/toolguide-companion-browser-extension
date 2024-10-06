9cf1a231767b1b0e37385c43c127557927221a93
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-10-06 06:15:35 +0300

    Add later to use REST api call to do things.

--- src/components/helper.ts
+++ src/components/helper.ts
@@ -11,3 +11,55 @@ export class Template {
         return template;
     }
 }
+
+// Function to get JSON data to be appended into DOM using REST
+
+/*
+
+interface AppVersionNumber {
+    version: string;
+}
+
+export class AppVersion {
+    public async getVersion(): Promise<AppVersionNumber[] | any> {
+        // User headers
+        const headers: Headers = new Headers();
+        headers.set("Content-Type", "application/json");
+        headers.set("Accept", "application/json");
+
+        // Create a request object
+        const request: RequestInfo = new Request("/manifest.json", {
+            method: "GET",
+            headers: headers
+        });
+
+        try {
+            const response = await fetch(request);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const jsonData: AppVersionNumber[] = await response.json();
+            return jsonData;
+        } catch (error) {
+            console.error("Could not retrieve JSON: ", error);
+            throw error;
+        }
+    }
+
+    // Function to fetch the version from getVersion() and update the DOM via async
+    public async fetchVersion(elem: HTMLElement | null) {
+        try {
+            const versionData = await this.getVersion();
+            if (versionData) {
+                const version = JSON.stringify(versionData); // Access the version property ONLY
+                if (elem) {
+                    elem.textContent = version;
+                }
+            }
+        } catch (error) {
+            console.error("Error fetching version:", error);
+        }
+    }
+}
+
+*/
