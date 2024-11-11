3e693e8ccc738229d30e785629efa6b22e3381ae
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-11-11 17:02:58 +0300

    Fix: unitConvert() function not working properly.

--- src/components/converters.ts
+++ src/components/converters.ts
@@ -120,7 +120,7 @@ export class Converters extends HTMLElement {
                 <div class="uc-container px-1">
                     <div class="uc-inputs input-group mb-3">
                         <span class="input-group-text" id="unitConversionInput">Input</span>
-                        <input type="text" class="form-control" placeholder="Type here..." aria-label="Type Input" aria-describedby="unitConversionInput"/>
+                        <input type="text" class="form-control ruc-form-control" placeholder="Type here..." aria-label="Type Input" aria-describedby="unitConversionInput"/>
                     </div>
                     <div class="uc-inputs-selection mb-3">
                         ${this.renderUnitConverterInputSelection()}
@@ -130,7 +130,7 @@ export class Converters extends HTMLElement {
                         <textarea class="uc-output-value w-100 form-control fs-3" id="rucOutputValue" title="Result" placeholder="Result" name="result" readonly></textarea>
                         <div class="alerts d-flex flex-row align-content-center justify-content-between">
                             <div>
-                                <button class="btn btn-discovery uc-convert-btn rounded-pill fs-4 mt-3" id="rucConvertBtn">Convert</button>
+                                <button class="btn btn-discovery ruc-convert-btn rounded-pill fs-4 mt-3" id="rucConvertBtn">Convert</button>
                             </div>
                             <div class="d-flex flex-row align-content-center justify-content-center w-100">
                                 <div class="ruc-alert alert alert-danger transition ease-in-out duration-300 mt-3 my-0 py-0 rounded-pill" role="alert" style="opacity: 0;">
@@ -150,7 +150,7 @@ export class Converters extends HTMLElement {
     private renderUnitConverterInputSelection(): string {
         return `
             <div class="ruc-selection-container d-flex flex-row align-items-center justify-content-center gap-2">
-                <div class="uc-child uc-one w-100">
+                <div class="ruc-child uc-one w-100">
                     <label for="ruc-value-one">From:</label>
                     <select class="form-select" aria-label="First Value Select" name="ruc-value-one" id="rucValueOne" title="First Value">
                         <option value="Millimeter">Millimeter</option>
@@ -161,7 +161,7 @@ export class Converters extends HTMLElement {
                 </div>
                 <div class="ruc-child uc-two w-100">
                     <label for="ruc-value-two">To:</label>
-                    <select class="form-select" aria-label="First Value Select" name="ruc-value-one" id="rucValueOne" title="First Value">
+                    <select class="form-select" aria-label="Second Value Select" name="ruc-value-two" id="rucValueTwo" title="Second Value">
                         <option value="Millimeter">Millimeter</option>
                         <option value="Centimeter">Centimeter</option>
                         <option value="Meter">Meter</option>
@@ -170,6 +170,47 @@ export class Converters extends HTMLElement {
                 </div>
             </div>
         `;
+    }
+
+    private unitConvert = (inputValue: number, from: string, to: string): number => {
+        if (typeof inputValue !== "number" && typeof from !== "string" && typeof to !== "string") {
+            throw new Error("Input value must be a number.");
+        }
+
+        // Define conversion values as base points to let calculation be performed
+        const conversionFactors: {
+            [unit1: string]: { [unit2: string]: number; };
+        } = {
+            "Millimeter": {
+                "Centimeter": 0.1,
+                "Meter": 0.001,
+                "Kilometer": 0.000001
+            },
+            "Centimeter": {
+                "Millimeter": 10,
+                "Meter": 0.01,
+                "Kilometer": 0.00001
+            },
+            "Meter": {
+                "Millimeter": 1000,
+                "Centimeter": 100,
+                "Kilometer": 0.001
+            },
+            "Kilometer": {
+                "Millimeter": 1000000,
+                "Centimeter": 100000,
+                "Meter": 1000
+            }
+        };
+
+        // Check if the conversion is possible
+        if (!conversionFactors[from] || !conversionFactors[from][to]) {
+            this.appCalculation.displayAlert(".ruc-alert", ".ruc-alert-message", "Conversion invalid.");
+            throw new Error("Conversion invalid.");
+        }
+
+        const convertedValue = inputValue * conversionFactors[from][to];
+        return convertedValue;
     }
 
     // Currency Converter
@@ -227,52 +268,6 @@ export class Converters extends HTMLElement {
             }
         }
 
-        // Unit Converter functions
-        // Define HTMLElements of Unit Converter
-        const rucInputField = document.querySelector(".ruc-form-control") as HTMLInputElement;
-        const rucOutputTextarea = document.getElementById("rucOutputValue") as HTMLTextAreaElement;
-        const rucConvertBtn = document.getElementById("rucConvertBtn") as HTMLButtonElement;
-        const rucValueOneSelect = document.getElementById("rucValueOne") as HTMLInputElement;
-        const rucValueTwoSelect = document.getElementById("rucValueTwo") as HTMLInputElement;
-
-        const unitConvert = (inputValue: number, from: string, to: string) => {
-            if (typeof inputValue !== "number" && typeof from !== "string" && typeof to !== "string") {
-                throw new Error("Input value must be a number.");
-            }
-
-            // Define conversion values to be calculated
-            const conversionFactors: {
-                [unit1: string]: { [unit2: string]: number; };
-            } = {
-                "Millimeter": {
-                    "Centimeter": 0.1,
-                    "Meter": 0.001,
-                    "Kilometer": 0.000001
-                },
-                "Centimeter": {
-                    "Millimeter": 10,
-                    "Meter": 0.01,
-                    "Kilometer": 0.00001
-                },
-                "Meter": {
-                    "Millimeter": 1000,
-                    "Centimeter": 100,
-                    "Kilometer": 0.001
-                },
-                "Kilometer": {
-                    "Millimeter": 1000000,
-                    "Centimeter": 100000,
-                    "Meter": 1000
-                }
-            };
-
-            // Check if the conversion is possible
-            if (!conversionFactors[from] || !conversionFactors[from][to]) {
-                this.appCalculation.displayAlert(".ruc-alert", ".ruc-alert-message", "Conversion invalid.");
-                throw new Error(`Conversion from ${from} to ${to} is not supported.`);
-            }
-        }
-
         // Add event listener to convert button, thus do the conversion
         convertBtn.addEventListener("click", () => {
             // First clean the input
@@ -296,8 +291,27 @@ export class Converters extends HTMLElement {
                 }
             }
         });
-    }
 
+        // Unit Converter functions
+        // Define HTMLElements of Unit Converter
+        const rucInputField = document.querySelector(".ruc-form-control") as HTMLInputElement;
+        const rucOutputTextarea = document.getElementById("rucOutputValue") as HTMLTextAreaElement;
+        const rucConvertBtn = document.getElementById("rucConvertBtn") as HTMLButtonElement;
+        const rucValueOneSelect = document.getElementById("rucValueOne") as HTMLInputElement;
+        const rucValueTwoSelect = document.getElementById("rucValueTwo") as HTMLInputElement;
+
+        rucConvertBtn.addEventListener("click", () => {
+            const rucIfParsed = parseFloat(rucInputField.value);
+            // Check if input is a number or not
+            if (isNaN(rucIfParsed)) {
+                this.appCalculation.displayAlert(".ruc-alert", ".ruc-alert-message", "Invalid input value.");
+                return;
+            }
+            // Perform the calculation
+            const convertedValue = this.unitConvert(rucIfParsed, rucValueOneSelect.value, rucValueTwoSelect.value);
+            const finalResult = rucOutputTextarea.value = convertedValue.toString();
+        });
+    }
 }
 
 customElements.define("app-converters", Converters);
