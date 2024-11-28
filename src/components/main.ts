ff51002cca3308d0f0406761ed55b01f5bec121a
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-11-28 21:06:37 +0300

    Move stylings to static content.

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,5 +1,6 @@
 // main.ts
-import { Template, STYLINGS } from "./helper.js";
+import { Template } from "./helper.js";
+import { ElementStyling } from "../static.js";
 import Converters from "./converters.js";
 import Generators from "./generators.js";
 import AppCalculations from "./appCalculations.js";
@@ -10,6 +11,7 @@ import SystemInformation from "./sys_info.js";
 
 export class Main extends HTMLElement {
     private templateHelper: Template;
+    private staticElementStylings: ElementStyling;
     private appCalculations: AppCalculations;
     private converters: Converters;
     private generators: Generators;
@@ -21,6 +23,7 @@ export class Main extends HTMLElement {
     constructor() {
         super();
         this.templateHelper = new Template();
+        this.staticElementStylings = new ElementStyling();
         this.appCalculations = new AppCalculations();
         this.converters = new Converters();
         this.generators = new Generators();
@@ -36,8 +39,8 @@ export class Main extends HTMLElement {
     // Render the main placeholder to host the actual elements here
     public renderContent(): string {
         return `
-        <main class="${STYLINGS.documentStyling.main}">
-            <div class="${STYLINGS.documentStyling.mainPlaceholder}">
+        <main class="${this.staticElementStylings.STYLINGS.documentStyling.main}">
+            <div class="${this.staticElementStylings.STYLINGS.documentStyling.mainPlaceholder}">
                 <img class="img-fluid intro-icon" src="/images/logo/final-icon-without-text.png" alt="App Icon Logo">
                 <h1>Toolguide Companion</h1>
                 <p>Your browser companion to accomplish many handy stuff!</p>
@@ -47,7 +50,7 @@ export class Main extends HTMLElement {
                     </a>
                 </div>
             </div>
-            <div id="${STYLINGS.ids.dynamicContent}"></div>
+            <div id="${this.staticElementStylings.STYLINGS.ids.dynamicContent}"></div>
         </main>
         `;
     }
@@ -104,8 +107,8 @@ export class Main extends HTMLElement {
 
     private renderDefault(): string {
         return `
-            <div class="${STYLINGS.welcome.div}">
-                <img class="welcome-message-logo" src="${STYLINGS.welcome.imgPath}">
+            <div class="${this.staticElementStylings.STYLINGS.welcome.div}">
+                <img class="welcome-message-logo img-fluid" src="${this.staticElementStylings.STYLINGS.welcome.imgPath}">
                 <h4>Section not available!</h4>
             </div>
         `;
